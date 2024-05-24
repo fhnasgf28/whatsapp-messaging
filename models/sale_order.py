a8132da76ac79f90d054587bdd4e400d99ae45c8
Farhan Assegaf <farhan.assegaf19019@student.unsika.ac.id>
2024-05-24 11:35:55 +0700

    [ADD]Portal Share

--- models/sale_order.py
+++ models/sale_order.py
@@ -34,41 +34,7 @@ class SaleOrder(models.Model):
             'target': 'new',
             'context': ctx,
         }
-
+    def check_customers(self, partner_ids):
+        pass
     def action_whatsapp_multi(self):
-        """Initiate WhatsApp messaging for multiple invoices and open a message
-        composition wizard."""
-
-        account_move_ids = self.env['account.move'].browse(self.env.context.get('active_ids'))
-        partner_ids = []
-        for account_move in account_move_ids:
-            partner_ids.append(account_move.partner_id.id)
-        partner_check = self.check_customer(partner_ids)
-        if partner_check:
-            account_move_numbers = account_move_ids.mapped('name')
-            account_move_numbers = "\n".join(account_move_numbers)
-            compose_form_id = self.env.ref('whatsapp_mail_messaging.whatsapp_send_message_view_form').id
-            ctx = dict(self.env.context)
-            message = ("Hi" + " " + self.partner_id.name + ',' + '\n' +
-                       "Your Orders are" + '\n' + account_move_numbers + ' ' + "is ready for review.Do not hesitate to contact us if "
-                           "you have any questions.")
-            ctx.update({
-                'default_message': message,
-                'default_partner_id': account_move_ids[0].partner_id.id,
-                'default_mobile': account_move_ids[0].partner_id.mobile,
-                'default_image_1920': account_move_ids[0].partner_id.image_1920,
-            })
-            return {
-                'type': 'ir.actions.act_window',
-                'view_mode': 'form',
-                'res_model': 'whatsapp.send.message',
-                'views': [(compose_form_id, 'form')],
-                'view_id': compose_form_id,
-                'target': 'new',
-                'context': ctx,
-            }
-        else:
-            raise UserError(_(
-                'It appears that you have selected orders from multiple'
-                ' customers. Please select orders from a single customer.'
-            ))
+       pass
---
+++ wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import portal_share, whatsapp_send_message
